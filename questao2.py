33b27cbb58c1c3c9690ddf80159173761c6faa13
Erick-sr <ericksoares.fis@gmail.com>
2025-08-20 07:50:19 -0300

    questão 2

---
+++ questao2.py
@@ -0,0 +1,15 @@
+#calculadora 
+
+numero1 = 12
+numero2 = 14
+
+soma = numero1 + numero2
+
+print(soma)
+
+num1 = float(input("insira o numero "))
+num2 = float(input("insira o segundo numero "))
+
+soma =  num1 + num2
+
+print(soma)
