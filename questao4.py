ba9b6af3618dd2f7ded68167bef82f13e785a8e9
Erick-sr <ericksoares.fis@gmail.com>
2025-08-20 08:13:45 -0300

    Questão 4

---
+++ questao4.py
@@ -0,0 +1,10 @@
+#variaveis do Produto 
+nome_produto = "cadeira infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+#calculo do valor total 
+
+preco_total = preco_unitario * quantidade
+
+print(f"o valor a pagar é R$ {preco_total:.2f}")
